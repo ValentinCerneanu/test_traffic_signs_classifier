b24013def0a8a456c2c1381758d87ce79715aa58
Alex Manolescu <alexandru.manolescu@softbinator.com>
2021-06-23 17:54:08 +0300

    save progress

---
+++ web/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.template import loader
+
+from .forms import MyForm
+
+class FormView(View):
+    def get(self, request):
+        template = loader.get_template('form.html')
+        context = {}
+
+        context['form'] = MyForm()
+
+        return render(request, 'form.html', context)
+
+    def post(self, request):
+        context = {}
+        form = MyForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.cleaned_data.get("name")
+            img = form.cleaned_data.get("image")
+            print(img)
+            print(name)
+
+        context['name'] = name
+
+        return render(request, 'form.html', context)
