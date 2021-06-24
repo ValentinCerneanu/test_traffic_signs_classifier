7698816d00fd3f21034f0517b6c57a1dd2027d08
Alex Manolescu <alexandru.manolescu@softbinator.com>
2021-06-24 11:43:54 +0300

    save progress

--- nnv/urls.py
+++ nnv/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from web.views import FormView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
